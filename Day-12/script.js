5cbe9a9b4d0a2132ef4fb1641e0c781c142e3e1a
Nikhilkumar3065 <obulshettynikhilkumar@gmail.com>
2026-05-01 19:36:36 +0530

    Add files via upload

---
+++ Day-12/script.js
@@ -0,0 +1,92 @@
+//  let data;
+//  data = 'nikhil';
+//  data = "nniikkhhiill";
+//  const data = 'nikhil';
+//  console.log(data);
+//  operators
+//  arthmetic operators  +,-,*,/,%,**
+//  assignment operators  =,+=,-=,*=,/=,%=,**=
+//  comparison operators  ><,<=,>=,==,===,!=,!==
+//  logical operators
+//  bitwise operators
+
+
+// let num1 = 10;
+//  let num2 = '20';
+//  console.log(num1 + num2);
+// let num1 = 10;
+// if (num1 == '100') {
+//     console.log("true1")
+// }
+// else if (num1 === '100')
+//     { console.log("true2") }
+// else {
+//     console.log("false")
+// }
+
+// console.log(num1);
+
+// let name = nikhil;
+// switch (name) {
+//     case hii: {
+//         console.log("hii");
+//         break;
+//     }
+//     case 'text': {
+//         console.log("falsethis is text 101");
+//         break;
+//     }
+//     case 'nikhil': {
+//         console.log("this is nikhil");
+//         break;
+//     }
+//     default: {
+//         console.log("false");
+//         break
+//     }
+// }
+// loop
+// for loop
+// while loop
+// do while loop
+// console.log("hii this _________");
+// for (let i = 0; i < 5; i++) {
+//     console.log("hii");
+// }
+// let i = 0;
+// while (i <= 5) {
+//     console.log("hi");
+//     i++;
+// }
+
+// let i = 1;
+// do {
+//     console.log("hii");
+//     i++;
+// } while (i <= 0) {
+//     console.log("hii");
+//     // i++;
+// }
+// dom
+// document object model
+// let h1text = (document.getElementsByTagName('h1'));
+//  h1text[0].innerHTML = "😊😂😘💕😍";
+// console.log(h1text);
+
+// 1st line-- tag
+// let h1text = (document.getElementsByTagName('h1'));
+// function start() {
+//     h1text[0].innerHTML = "This is NIKHILL";
+// }
+
+// second line -- class
+// let h1text = (document.getElementsByClassName('text'));
+// function start() {
+//     h1text[0].innerHTML = "This is NIKHIL";
+// }
+
+// 3rd line-- id
+let h1text = (document.getElementById('text'));
+function start() {
+    h1text.innerHTML = "This is NIKHIL";
+}
